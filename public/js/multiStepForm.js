0feceee61ef3edeed0c89c5e16e54379e2d0d691
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-09-28 09:49:45 +0200

    multi step form functionality

--- public/js/multiStepForm.js
+++ public/js/multiStepForm.js
@@ -2,10 +2,39 @@ const next = document.querySelectorAll('.next');
 const prev = document.querySelectorAll('.prev');
 const progress = document.querySelector('.progress');
 const steps = document.querySelectorAll('.step')
+const progressSteps = document.querySelectorAll('.progress-step');
+
+let stepCount = 0;
 
 next.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    console.log('funguje to')
+    stepCount++;
+    updateSteps();
+    updateProgressBar();
   })
 })
 
+prev.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    stepCount--;
+    updateSteps();
+    updateProgressBar();
+  })
+})
+
+const updateSteps = () => {
+  steps.forEach(step => {
+    step.classList.contains('active') && step.classList.remove('active');
+  })
+  steps[stepCount].classList.add('active');
+}
+
+const updateProgressBar = () => {
+  progressSteps.forEach((step, idx) => {
+    if (idx <= stepCount) {
+      step.classList.add('step-active')
+    } else {
+      step.classList.remove('step-active')
+    }
+  })
+}
--- resources/views/shipping/create.blade.php
+++ resources/views/shipping/create.blade.php
@@ -1,27 +1,33 @@
 <x-app-layout>
     <div class="flex justify-center">
-        <form method="POST" action="" class="max-w-3xl ml-72">
-            <div class="progress-bar relative flex justify-between mb-12">
+        <form method="POST" action="" class="w-160 ml-72">
+            <div class="progress-bar relative flex justify-between mb-20">
                 <div class="progress relative flex justify-between" id="progress"></div>
-                <div data-title="Fakturačné údaje"
-                    class="step-active flex items-center justify-center w-9 h-9 rounded-full border z-10">
+                <div class="step-active progress-step flex items-center justify-center w-9 h-9 rounded-full border z-10">
                     1
                     <div class="absolute top-12 -left-8 text-black w-36 font-semibold">
                         Fakturačné údaje
                     </div>
                 </div>
-                <div data-title="Doprava"
-                    class="flex items-center justify-center w-9 h-9 rounded-full border-2 border-black text-black bg-white z-10">
+                <div
+                    class="progress-step flex items-center justify-center w-9 h-9 rounded-full border-2 border-black text-black bg-white z-10">
                     2
-                    <div class="absolute top-12 left-60 text-black w-36 font-semibold">
+                    <div class="absolute top-12 left-48 text-black w-36 font-semibold">
                         Doprava
                     </div>
                 </div>
-                <div data-title="Platba"
-                    class="flex items-center justify-center w-9 h-9 rounded-full border-2 border-black text-black bg-white z-10">
+                <div
+                    class="progress-step flex items-center justify-center w-9 h-9 rounded-full border-2 border-black text-black bg-white z-10">
                     3
-                    <div class="absolute top-12 -right-2 text-black w-36 font-semibold text-right">
+                    <div class="absolute top-12 right-48  text-black w-36 font-semibold text-right">
                         Platba
+                    </div>
+                </div>
+                <div
+                    class="progress-step flex items-center justify-center w-9 h-9 rounded-full border-2 border-black text-black bg-white z-10">
+                    4
+                    <div class="absolute top-12 -right-12 text-black w-36 font-semibold text-right">
+                        Súhrn objednávky
                     </div>
                 </div>
             </div>
@@ -58,7 +64,6 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
-
                     </button>
                 </div>
             </div>
@@ -130,6 +135,102 @@
                     </div>
                 </button>
                 <div class="packeta-selector-value text-gray-500 font-semibold text-sm mt-4">
+                </div>
+                <div class="flex items-center justify-between mt-8">
+                    <button type="button"
+                        class="prev flex items-center bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                        </svg>
+                        Fakturačné údaje
+                    </button>
+                    <button type="button"
+                        class="next flex items-center bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        Platba
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="step">
+                <button type="button"
+                    class="border rounded-lg px-2 py-5 mt-4 font-semibold text-lg w-full hover:border-gray-700 hover:border-2 transition ease-in duration-200">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <input type="radio" name="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-700">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                            </svg>
+                            <div>
+                                Dobierka
+                            </div>
+                        </div>
+                        <div class="mr-6 text-md">
+                            1 €
+                        </div>
+                    </div>
+                </button>
+                <button type="button"
+                    class="border rounded-lg px-2 py-5 mt-4 font-semibold text-lg w-full hover:border-gray-700 hover:border-2 transition ease-in duration-200">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <input type="radio" name="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-700">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                            </svg>
+                            <div>
+                                Platba kartou
+                            </div>
+                        </div>
+                        <div class="mr-6 text-md">
+                            Zadarmo
+                        </div>
+                    </div>
+                </button>
+                <div class="flex items-center justify-between mt-8">
+                    <button type="button"
+                        class="prev flex items-center bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                        </svg>
+                        Doprava
+                    </button>
+                    <button type="button"
+                        class="next flex items-center bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        Súhrn objednávky
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="step">
+                <div class="flex items-center justify-between mt-8">
+                    <button type="button"
+                        class="prev flex items-center bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                        </svg>
+                        Platba
+                    </button>
+                    <button type="button" class="bg-dark-navy-blue text-white rounded-lg px-6 py-3">
+                        Potvrdiť objendávku
+                    </button>
                 </div>
             </div>
         </form>
